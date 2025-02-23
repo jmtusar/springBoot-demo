cfe6c76c77e91e8408dcb11f81555ec20130542d
Jobar Mahbob Tusar <jmtusar@gmail.com>
2025-02-23 11:33:28 +0600

    implementing DAO layer

--- src/main/java/com/jmtusar/springDemo/student/StudentController.java
+++ src/main/java/com/jmtusar/springDemo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.jmtusar.springDemo.student;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -24,9 +22,29 @@ public class StudentController {
         return service.save(student);
     }
 
+    @GetMapping("/{email}")
+    public Student findByEmail(
+            @PathVariable("email") String email
+    ) {
+        return service.findByEmail(email);
+    }
+
     @GetMapping
     public List<Student> findAllStudents(){
         return service.findAllStudents();
     }
 
+    @PutMapping
+    public Student updateStudent(
+            @RequestBody Student student
+    ){
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete(
+            @PathVariable("email") String email
+    ){
+        service.delete(email);
+    }
 }
